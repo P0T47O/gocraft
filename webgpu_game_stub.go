e60b8985cb0f9de62a89df378b0178a5bbf785ca
P0T47O <47151219+P0T47O@users.noreply.github.com>
2026-09-15 21:55:30 +0800

    feat: add non-Windows WebGPU game stub

---
+++ webgpu_game_stub.go
@@ -0,0 +1,15 @@
+//go:build !windows
+
+package main
+
+import "fmt"
+
+func ensureExperimentalWebGPURenderer() error {
+	return fmt.Errorf("experimental WebGPU game renderer currently supports Windows only")
+}
+
+func drawExperimentalWebGPUFrame() error {
+	return fmt.Errorf("experimental WebGPU game renderer currently supports Windows only")
+}
+
+func closeExperimentalWebGPURenderer() {}
